e313ec94bdb1c2ef52d0e79c3bc833004f4c90e5
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2025-12-13 16:58:27 +0800

    Change log file for chnroute updates
    Updated logging to use ovpn-script.log instead of update-ip.log.

--- files/etc/openvpn/openvpn-up.sh
+++ files/etc/openvpn/openvpn-up.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DATE=`date +%Y-%m-%d-%H:%M:%S`
+
 #add route-policy
 src=$(ip addr show tun0 | grep 'inet ' | sed 's/^.*inet //g' | sed 's/\/16.*$//g')
 gw=$(ip addr show tun0 | grep 'inet ' | cut -d' ' -f6 | cut -d'.' -f1-2).0.1
@@ -8,7 +10,6 @@ ip rule add from all fwmark 1 table 100
 #ip route add 8.8.8.8 via $gw dev tun0 src $src
 
 #check and add rules of mangle     
-DATE=`date +%Y-%m-%d-%H:%M:%S`
 iptables -t mangle -C PREROUTING -m set ! --match-set chnroute dst -j MARK --set-mark 1
 if [ $? = 0 ]; then
         echo "$DATE: The PREROUTING of mangle rules alread exist" 
@@ -56,7 +57,7 @@ EOF
 
 if ! is_private_ip "$ips"; then
     echo "add chnroute $ips" >> /root/script/chnroute-ipset
-    echo "Add $ips to the chnroute seccessfully" >>/root/script/update-ip.log
+    echo "Add $ips to the chnroute seccessfully" >>/root/script/ovpn-script.log
 else
-    echo "The $ips is a private ip address" >>/root/script/update-ip.log
+    echo "The $ips is a private ip address" >>/root/script/ovpn-script.log
 fi
